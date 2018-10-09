cc21fedf0820db71b6ac05f7708f027a6419d14f
mlesny <mikolaj.lesny@olx.pl>
2018-10-09 11:54:35 +0200

    Add comments.

--- naive_algorithm.py
+++ naive_algorithm.py
@@ -1,7 +1,7 @@
 import math
 
-k = 7
-h = 0.2
+k = 0
+h = 0.6
 
 number_of_instances = []
 current_instace = -1
@@ -24,16 +24,20 @@ print("Sum of p(i): " + str(p_sum))
 
 d = round(p_sum * float(h))
 print("d: " + str(d))
-e_ordered = sorted(instance[k], key = lambda x: int(x[1]) - int(x[2])) #sorted by early - tard
+# We calculate difference in penalty for earliness and tardiness (b(i) - a(i)) and take first tasks with least value.
+e_ordered = sorted(instance[k], key = lambda x: int(x[1]) - int(x[2])) # Sorted tasks (accordingly to above formula)
 # print(e_ordered)
 penalty = 0
-time = 0
+current_time = 0
+# Penatly is calculated: abs(current_time - time_task_finished) * a(i)) if current_time < d
+#                    or: abs(current_time - time_task_finished) * b(i)) if current_time > d
 for i in e_ordered:
-    time += int(i[0])
-    if time < d:
-        penalty += int(i[1]) * abs(time - d)
-    if time > d:
-        penalty += int(i[2]) * abs(time - d)
+    current_time += int(i[0])
+    print("Current time: {}  p(i): {} a(i): {}  b(i): {}".format(current_time, i[0], i[1], i[2]))
+    if current_time < d:
+        penalty += int(i[1]) * abs(current_time - d)
+    if current_time > d:
+        penalty += int(i[2]) * abs(current_time - d)
 print("Penalty: " + str(penalty))
 
 
